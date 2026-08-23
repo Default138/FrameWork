1f528e26658af5379505d13d8ffce5ae44d0f3c4
Rafael Camargo <rc0102725@gmail.com>
2026-08-23 20:17:43 -0300

    Delete criadorDeClasses directory

--- criadorDeClasses/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include("banco.php");
-include("criadorDeClasses.php");
-$entidades = buscarTabelas($entidades);
-  print_r($entidades);
- $atributos = buscarAtributos("aluno");
- print_r($atributos);
